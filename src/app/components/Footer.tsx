13986c0fedaa7568fa66540ec5ee626f428f06e4
dipesh162 <dipeshsingh162@gmail.com>
2023-12-27 20:41:06 +0530

    Completed Footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,13 +1,21 @@
+import { LiaLinkedinIn } from "react-icons/lia";
+import { FaGithub } from "react-icons/fa";
+import Link from "next/link";
+
 export default function Footer(){
     return(
-        <footer className="bg-[#2d2e32] py-[5rem] px-0 w-full text-[#fff]">
-            <div className="mx-auto max-w-[107rem] flex justify-between">
-                <h3 className="text-[1.7rem]">
+        <footer className="bg-[#2d2e32] py-[2rem] px-0 w-full text-[#fff]">
+            <div className="gap-[14px] mx-auto max-w-[107rem] flex-col md:flex-row flex px-[50px] justify-between">
+                <h3 className="text-center text-[1rem] font-bold justify-center font-poppins tracking-[0.5px]">
                     Copyright © 2023. All rights are reserved
                 </h3>
-                <div>
-                    <h4>linkedin icon</h4>
-                    <h4>github icon</h4>
+                <div className="flex gap-3 justify-center">
+                    <Link href='https://www.linkedin.com/in/dipesh162/'>
+                        <LiaLinkedinIn size={32} />
+                    </Link>
+                    <Link href='https://github.com/dipesh162'>
+                        <FaGithub size={30} />
+                    </Link>
                 </div>
             </div>
         </footer>
