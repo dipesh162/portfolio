fce9ed7104be2cea197876c0d0c3ca2377b5ec8a
dipesh162 <dipeshsingh162@gmail.com>
2023-12-30 14:08:12 +0530

    Updated svg

--- src/app/components/Sections/Contact.tsx
+++ src/app/components/Sections/Contact.tsx
@@ -19,7 +19,7 @@ function Contact() {
             </div>
             <div className='flex flex-col md:flex-row gap-[1rem] items-center'>
               <div className='rounded-[50%] justify-center items-center bg-white text-[#147efb] flex w-[50px] h-[50px]' style={{boxShadow:'0 0 10px rgba(0,0,0,.1)'}}>
-              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="tabler-icon tabler-icon-map-search"><path d="M11 18l-2 -1l-6 3v-13l6 -3l6 3l6 -3v7.5"></path><path d="M9 4v13"></path><path d="M15 7v5"></path><path d="M18 18m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0"></path><path d="M20.2 20.2l1.8 1.8"></path></svg>
+              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="tabler-icon tabler-icon-mail"><path d="M3 7a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-10z"></path><path d="M3 7l9 6l9 -6"></path></svg>
               </div>
               <div className='flex flex-col gap-[2px] items-center md:items-baseline'>
                 <h3 className='text-[#2d2e32] font-bold'>Mail</h3>
